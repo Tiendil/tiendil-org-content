0f9e1193039c09cb490add58505707a4792db6b3
Aliaksei Yaletski (Tiendil) <a.eletsky@gmail.com>
2026-03-03 12:32:13 +0100

    upgrade brigid to 0.16.0

--- local/bin/dev-server.sh
+++ local/bin/dev-server.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/bash
 
-poetry run uvicorn brigid.application.application:app --host 0.0.0.0 --port 8000 --workers 1 --reload --reload-dir '../content'  --reload-include '*'
+poetry run uvicorn brigid.asgi:app --host 0.0.0.0 --port 8000 --workers 1 --reload --reload-dir '../content'  --reload-include '*'
